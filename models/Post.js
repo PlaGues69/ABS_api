7ac0a560eed177d49fb259166bbbacf25c38a537
Plagues <plaguesisgod@gmail.com>
2025-08-01 23:19:05 +0545

    Add Mongoose models for users and products

---
+++ models/Post.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  comments: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }]
+}, {
+  timestamps: true
+});
+
+// Add text index for search functionality
+postSchema.index({ title: 'text', content: 'text' });
+
+module.exports = mongoose.model('Post', postSchema);
